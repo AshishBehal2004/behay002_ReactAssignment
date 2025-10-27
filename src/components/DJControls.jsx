dbbdc45a9a291c8899986cbccc26f81d44843771
behay002 <behay002@mymail.unisa.edu.au>
2025-10-28 08:24:31 +1030

    Working on wiring the logic

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -1,31 +1,31 @@
 function DjControls() {
     return (
         <>
-            <div class="input-group mb-3">
+            <div className="input-group mb-3">
                 <span className="input-group-text" id="cpmLabel">SetCpm</span>
 
                 <input type="text" className="form-control" id="cpm_textInput" placeholder="120" aria-label="cpm" aria-describedby="cpmLabel"></input>
 
             </div>
-            <label for="volumeSlider" className="form-label">Volume</label>
+            <label htmlFor="volumeSlider" className="form-label">Volume</label>
             <input type="range" className="form-range" min="0" max="1" step="0.01" id="volumeSlider"></input>
 
-            <div class="form-check">
+            <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="instrument_1"></input>
-                <label className="form-check-label" for="instrument_1">
+                <label className="form-check-label" htmlFor="instrument_1">
                         Instrument 1
                     </label>
             </div>
 
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="instrument_2" ></input>
-                <label className="form-check-label" for="instrument_2">
+                <label className="form-check-label" htmlFor="instrument_2">
                     Instrument 2
                     </label>
             </div>
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="instrument_3" ></input>
-                <label className="form-check-label" for="instrument_3">
+                <label className="form-check-label" htmlFor="instrument_3">
                     Instrument 3
                     </label>
             </div>
