f86ac102748a9873bd89667bd093f42d6c9f555d
behay002 <behay002@mymail.unisa.edu.au>
2025-10-28 21:24:12 +1030

    Re Adding Changes
    Forgot to do the commit after adding features, lost some of it. Hoping, this fix it.

--- src/App.js
+++ src/App.js
@@ -13,148 +13,149 @@ import DjControls from './components/DJControls';
 import PlayButtons from './components/PlayButtons';
 import ProcButtons from './components/ProcButtons';
 import PreProcessTextBody from './components/PreProcessTextBody';
-
+import { getD3Data } from './console-monkey-patch';
 let globalEditor = null;
 
 const handleD3Data = (event) => {
-    console.log(event.detail);
-};
+    export const handleD3Data = (event) => {
+        console.log(event.detail);
+    };
 
-//export function SetupButtons() {
+    //export function SetupButtons() {
 
-//    document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-//    document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-//    document.getElementById('process').addEventListener('click', () => {
-//        Proc()
-//    }
-//    )
-//    document.getElementById('process_play').addEventListener('click', () => {
-//        if (globalEditor != null) {
-//            Proc()
-//            globalEditor.evaluate()
-//        }
-//    }
-//    )
-//}
+    //    document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
+    //    document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
+    //    document.getElementById('process').addEventListener('click', () => {
+    //        Proc()
+    //    }
+    //    )
+    //    document.getElementById('process_play').addEventListener('click', () => {
+    //        if (globalEditor != null) {
+    //            Proc()
+    //            globalEditor.evaluate()
+    //        }
+    //    }
+    //    )
+    //}
 
 
 
-//export function ProcAndPlay() {
-//    if (globalEditor != null && globalEditor.repl.state.started == true) {
-//        console.log(globalEditor)
-//        Proc()
-//        globalEditor.evaluate();
-//    }
-//}
+    //export function ProcAndPlay() {
+    //    if (globalEditor != null && globalEditor.repl.state.started == true) {
+    //        console.log(globalEditor)
+    //        Proc()
+    //        globalEditor.evaluate();
+    //    }
+    //}
 
-//export function Proc() {
+    //export function Proc() {
 
-//    let proc_text = document.getElementById('proc').value
-//    let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
-//    ProcessText(proc_text);
-//    globalEditor.setCode(proc_text_replaced)
-//}
+    //    let proc_text = document.getElementById('proc').value
+    //    let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
+    //    ProcessText(proc_text);
+    //    globalEditor.setCode(proc_text_replaced)
+    //}
 
-//export function ProcessText(match, ...args) {
+    //export function ProcessText(match, ...args) {
 
-//    let replace = ""
-//    //if (document.getElementById('flexRadioDefault2').checked) {
-//    //    replace = "_"
-//    //}
+    //    let replace = ""
+    //    //if (document.getElementById('flexRadioDefault2').checked) {
+    //    //    replace = "_"
+    //    //}
 
-//    return replace
-//}
+    //    return replace
+    //}
 
-export default function StrudelDemo() {
+    export default function StrudelDemo() {
 
-    const hasRun = useRef(false);
+        const hasRun = useRef(false);
 
-    //defined two variables which are now going to be used for managing play and stop buttons 
-    const handlePlay = () => {
-        //Plays the code
-        globalEditor.evaluate();
+        //defined two variables which are now going to be used for managing play and stop buttons 
+        const handlePlay = () => {
+            //Plays the code
+            globalEditor.evaluate();
+        }
+
+        const handleStop = () => {
+            globalEditor.stop();
+        }
+        const [songText, setSongText] = useState(stranger_tune)
+
+
+        useEffect(() => {
+
+            if (!hasRun.current) {
+                document.addEventListener("d3Data", handleD3Data);
+                console_monkey_patch();
+                hasRun.current = true;
+                //Code copied from example: https://codeberg.org/uzu/strudel/src/branch/main/examples/codemirror-repl
+                //init canvas
+                const canvas = document.getElementById('roll');
+                canvas.width = canvas.width * 2;
+                canvas.height = canvas.height * 2;
+                const drawContext = canvas.getContext('2d');
+                const drawTime = [-2, 2]; // time window of drawn haps
+                globalEditor = new StrudelMirror({
+                    defaultOutput: webaudioOutput,
+                    getTime: () => getAudioContext().currentTime,
+                    transpiler,
+                    root: document.getElementById('editor'),
+                    drawTime,
+                    onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
+                    prebake: async () => {
+                        initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
+                        const loadModules = evalScope(
+                            import('@strudel/core'),
+                            import('@strudel/draw'),
+                            import('@strudel/mini'),
+                            import('@strudel/tonal'),
+                            import('@strudel/webaudio'),
+                        );
+                        await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+                    },
+                });
+
+                document.getElementById('proc').value = stranger_tune
+                //SetupButtons()
+                //Proc()
+            }
+            globalEditor.setCode(songText);
+        }, [songText]);
+
+
+        return (
+            <div>
+                <h2>Strudel Demo</h2>
+                <main>
+
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                                <PreProcessTextBody defaultValue={songText} onChange={(e) => setSongText(e.target.value)} />
+                            </div>
+                            <div className="col-md-4">
+
+                                <nav>
+                                    <ProcButtons />
+                                    <br />
+                                    <PlayButtons onPlay={handlePlay} onStop={handleStop} />
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                                <div id="editor" />
+                                <div id="output" />
+                            </div>
+                            <div className="col-md-4">
+                                <DjControls />
+                            </div>
+                        </div>
+                    </div>
+                    <canvas id="roll"></canvas>
+                </main >
+            </div >
+        );
+
+
     }
-
-    const handleStop = () => {
-        globalEditor.stop();
-    }
-    const [songText, setSongText] = useState(stranger_tune)
-
-
-useEffect(() => {
-
-    if (!hasRun.current) {
-        document.addEventListener("d3Data", handleD3Data);
-        console_monkey_patch();
-        hasRun.current = true;
-        //Code copied from example: https://codeberg.org/uzu/strudel/src/branch/main/examples/codemirror-repl
-            //init canvas
-            const canvas = document.getElementById('roll');
-            canvas.width = canvas.width * 2;
-            canvas.height = canvas.height * 2;
-            const drawContext = canvas.getContext('2d');
-            const drawTime = [-2, 2]; // time window of drawn haps
-            globalEditor = new StrudelMirror({
-                defaultOutput: webaudioOutput,
-                getTime: () => getAudioContext().currentTime,
-                transpiler,
-                root: document.getElementById('editor'),
-                drawTime,
-                onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
-                prebake: async () => {
-                    initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
-                    const loadModules = evalScope(
-                        import('@strudel/core'),
-                        import('@strudel/draw'),
-                        import('@strudel/mini'),
-                        import('@strudel/tonal'),
-                        import('@strudel/webaudio'),
-                    );
-                    await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
-                },
-            });
-            
-        document.getElementById('proc').value = stranger_tune
-        //SetupButtons()
-        //Proc()
-    }
-    globalEditor.setCode(songText);
-}, [songText]);
-
-
-return (
-    <div>
-        <h2>Strudel Demo</h2>
-        <main>
-
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <PreProcessTextBody defaultValue={songText} onChange={(e) => setSongText(e.target.value)} />
-                    </div>
-                    <div className="col-md-4">
-
-                        <nav>
-                            <ProcButtons/>
-                            <br />
-                            <PlayButtons onPlay={handlePlay} onStop={handleStop} />
-                        </nav>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <div id="editor" />
-                        <div id="output" />
-                    </div>
-                    <div className="col-md-4">
-                        <DjControls/>
-                    </div>
-                </div>
-            </div>
-            <canvas id="roll"></canvas>
-        </main >
-    </div >
-);
-
-
-}
